06e0dca9484f2487789f98c0379200241565ab12
José Luis Ruiz Ruiz <jose@wazuh.com>
2018-04-06 11:01:21 -0400

    Fixing wazuh metadata.rb to improve supermarket score

--- wazuh/metadata.rb
+++ wazuh/metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'info@wazuh.com'
 license          'Apache 2.0'
 description      'Installs and onfigures ossec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.5'
+issues_url 'https://github.com/wazuh/wazuh-chef/issues'
+source_url 'https://github.com/wazuh/wazuh-chef/tree/master/wazuh'
+version          '0.0.6'
 
 %w( apt ).each do |pkg|
   depends pkg
