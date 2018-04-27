6e913b35e9edb324a6aa5a098d90ffeb98bfc81c
Pedro S <snaowxt@gmail.com>
2018-04-27 20:24:33 +0200

    Bump to v0.0.9. Support Amazon Linux

--- wazuh/CHANGELOG.md
+++ wazuh/CHANGELOG.md
@@ -1,3 +1,5 @@
+## v0.0.9
+Amazon Linux OS included in repository.rb
 ## v0.0.8
 Fixed bugs in ossec_to_xml #4 Thanks @smith3v
 ## v0.0.1
--- wazuh/metadata.rb
+++ wazuh/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@wazuh.com'
 license          'Apache 2.0'
 description      'Installs and onfigures ossec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.8'
+version          '0.0.9'
 
 %w(redhat centos oracle).each do |el|
   supports el, '>= 6.0'
