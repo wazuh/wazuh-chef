03442f1fa175b92ed2a4aa3abd924d41e42e1ac5
Jose M <jm.garcia@wazuh.com>
2019-05-16 17:53:45 +0200

    Added default.rb recipes

--- cookbooks/wazuh_filebeat/recipes/default.rb
+++ cookbooks/wazuh_filebeat/recipes/default.rb
@@ -3,6 +3,4 @@
 # Recipe:: default
 # Author:: Wazuh <info@wazuh.com>
 
-include_recipe 'wazuh_filebeat::repository'
-
 include_recipe 'wazuh_filebeat::filebeat'
---
+++ cookbooks/wazuh_manager/recipes/default.rb
@@ -0,0 +1,1 @@
+include_recipe 'wazuh_manager::manager'
