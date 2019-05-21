1cff763cf8970593c759fb97afd7e12ade9da782
Jose M <jm.garcia@wazuh.com>
2019-05-21 18:11:30 +0200

    Deleted comments.

--- cookbooks/wazuh_elastic/attributes/elasticsearch.rb
+++ cookbooks/wazuh_elastic/attributes/elasticsearch.rb
@@ -3,4 +3,3 @@ default['wazuh-elastic']['elasticsearch_cluster_name'] = 'wazuh'
 default['wazuh-elastic']['elasticsearch_node_name'] = 'elk.wazuh-test.com'
 default['wazuh-elastic']['elasticsearch_port'] = 9200
 default['wazuh-elastic']['elasticsearch_ip'] = 'localhost'
-# default['wazuh-elastic']['elasticsearch_network_host'] = '127.0.0.1'
