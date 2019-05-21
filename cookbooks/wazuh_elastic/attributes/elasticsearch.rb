83d71348371e09c83390d5287fb39a7ada11b805
Jose M <jm.garcia@wazuh.com>
2019-05-21 18:00:28 +0200

    Fixed RAM problems and updated Curl URLs.

--- cookbooks/wazuh_elastic/attributes/elasticsearch.rb
+++ cookbooks/wazuh_elastic/attributes/elasticsearch.rb
@@ -1,4 +1,6 @@
+default['wazuh-elastic']['elasticsearch_memmory'] = "1g"
 default['wazuh-elastic']['elasticsearch_cluster_name'] = 'wazuh'
 default['wazuh-elastic']['elasticsearch_node_name'] = 'elk.wazuh-test.com'
 default['wazuh-elastic']['elasticsearch_port'] = 9200
 default['wazuh-elastic']['elasticsearch_ip'] = 'localhost'
+# default['wazuh-elastic']['elasticsearch_network_host'] = '127.0.0.1'
