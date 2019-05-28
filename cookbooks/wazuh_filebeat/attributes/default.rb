398254ec6964cb860a0f589aae369eda059f0324
Jose M <jm.garcia@wazuh.com>
2019-05-28 13:01:43 +0200

    Updated filebeat attributes

--- cookbooks/wazuh_filebeat/attributes/default.rb
+++ cookbooks/wazuh_filebeat/attributes/default.rb
@@ -5,12 +5,10 @@
 #
 #
 #
-default['filebeat']['elastic_stack_version'] = '6.8.0'
+default['filebeat']['elastic_stack_version'] = '7.1.0'
 default['filebeat']['package_name'] = 'filebeat'
 default['filebeat']['service_name'] = 'filebeat'
-default['filebeat']['logstash_servers'] = ["YOUR_ELASTIC_SERVER_IP:5000"]
+default['filebeat']['elasticsearch_server_ip'] = "172.19.0.211"
 default['filebeat']['timeout'] = 15
 default['filebeat']['config_path'] = '/etc/filebeat/filebeat.yml'
-default['filebeat']['certificate_authorities'] = '/etc/filebeat/logstash_certificate.crt'
-default['filebeat']['certificate'] = '/etc/filebeat/logstash.crt'
-default['filebeat']['key'] = '/etc/filebeat/logstash.key'
+
---
+++ cookbooks/wazuh_filebeat/attributes/versions.rb
@@ -0,0 +1,4 @@
+default['filebeat']['elastic_stack_version'] = '7.1.0'
+default['filebeat']['wazuh_app_version'] = "3.9.1_7.1.0"
+default['filebeat']['extensions_version'] = "v3.9.1"
+
