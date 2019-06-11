a88452bb86c2aaaef28832f7e2183f700ec2bdff
Jose M <jm.garcia@wazuh.com>
2019-06-11 14:39:08 +0200

    Update Agent attributes to adapt "ossec.conf" generation

--- cookbooks/wazuh_agent/attributes/localfile.rb
+++ cookbooks/wazuh_agent/attributes/localfile.rb
@@ -1,4 +1,6 @@
-default['ossec']['conf']['localfile'] = [
+
+if platform_family?('ubuntu', 'debian')
+  default['ossec']['conf']['localfile'] = [
     {
       'log_format' => 'command',
       'command' => 'df -P',
@@ -17,12 +19,12 @@ default['ossec']['conf']['localfile'] = [
       'log_format' => 'full_command',
       'command' => 'last -n 20',
       'frequency' => 360
-       }
+      }
     },
     {
       'content!' => {
         'log_format' => 'syslog',
-        'command' => '/var/ossec/logs/active-responses.log',
+        'location' => '/var/ossec/logs/active-responses.log',
       }
     },
     {
@@ -50,9 +52,47 @@ default['ossec']['conf']['localfile'] = [
         }
     }
   ]
-
-
-
-
-
-
+elsif platform_family?('rhel','centos', 'amazon')
+  default['ossec']['conf']['localfile'] = [
+    {
+      'log_format' => 'command',
+      'command' => 'df -P',
+      'frequency' => 360
+    },
+    {
+      'content!' => {
+        'log_format' => 'full_command',
+        'command' => "netstat -tulpn | sed 's/\([[:alnum:]]\+\)\ \+[[:digit:]]\+\ \+[[:digit:]]\+\ \+\(.*\):\([[:digit:]]*\)\ \+\([0-9\.\:\*]\+\).\+\ \([[:digit:]]*\/[[:alnum:]\-]*\).*/\1 \2 == \3 == \4 \5/' | sort -k 4 -g | sed 's/ == \(.*\) ==/:\1/' | sed 1,2d",
+        'alias' => 'netstat listening ports',
+        'frequency' => 360
+      }
+    },
+    {
+      'content!' => {
+      'log_format' => 'full_command',
+      'command' => 'last -n 20',
+      'frequency' => 360
+      }
+    },
+    {
+      'content!' => {
+        'log_format' => 'syslog',
+        'location' => '/var/ossec/logs/active-responses.log',
+      }
+    },
+    {
+      'content!' => {
+        'log_format' => 'syslog',
+        'location' => '/var/log/messages'
+        }
+    },
+    {
+      'content!' => {
+        'log_format' => 'syslog',
+        'location' => '/var/log/secure'
+        }
+    },
+  ]
+else
+  raise "Currently platforn not supported yet. Feel free to open an issue on https://www.github.com/wazuh/wazuh-chef if you consider that support for a specific OS should be added"
+end
