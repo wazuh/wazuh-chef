e0603b38480e3c4fcd4535c27003143b9365c6da
Jose M <jm.garcia@wazuh.com>
2019-06-12 11:10:53 +0200

    Bump version

--- cookbooks/wazuh_elastic/attributes/versions.rb
+++ cookbooks/wazuh_elastic/attributes/versions.rb
@@ -1,3 +1,3 @@
-default['wazuh-elastic']['elastic_stack_version'] = '7.1.0'
-default['wazuh-elastic']['wazuh_app_version'] = "3.9.1_7.1.0"
-default['wazuh-elastic']['extensions_version'] = "v3.9.1"
+default['wazuh-elastic']['elastic_stack_version'] = '7.1.1'
+default['wazuh-elastic']['wazuh_app_version'] = "3.9.2_7.1.1"
+default['wazuh-elastic']['extensions_version'] = "v3.9.2"
--- cookbooks/wazuh_filebeat/attributes/versions.rb
+++ cookbooks/wazuh_filebeat/attributes/versions.rb
@@ -1,4 +1,4 @@
-default['filebeat']['elastic_stack_version'] = '7.1.0'
-default['filebeat']['wazuh_app_version'] = "3.9.1_7.1.0"
-default['filebeat']['extensions_version'] = "v3.9.1"
+default['filebeat']['elastic_stack_version'] = '7.1.1'
+default['filebeat']['wazuh_app_version'] = "3.9.2_7.1.1"
+default['filebeat']['extensions_version'] = "v3.9.2"
 
