01caf98e151b4b29bc423347e7854e6ec30568bf
rshad <rashzk95@gmail.com>
2019-08-06 11:32:04 +0000

    improved run.sh, now it run over an array of distribution with argument usage possiblity

--- cookbooks/wazuh_manager/run.sh
+++ cookbooks/wazuh_manager/run.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-echo "Kitchen is creating the new instances"
-kitchen create ubuntu
-
-echo "Getting Wazuh managers IPs to the agents"
-ubuntu_manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep  ubuntu | grep manager`)"
-
-
 development_agent_path="$COOKBOOKS_HOME/wazuh_agent/test/environments/development.json"
 template=".template"
 
-cp "$development_agent_path$template" "$development_agent_path"
+if [ -z "$1" ]
+then
+	distributions=( "ubuntu" "centos" )
+else
+	distributions=$1
+fi
 
-echo "Assigning Wazuh managers IPs to the corresponding agents."
-sed -i 's/manager-client.wazuh-test.com//g' $development_agent_path
-sed -i 's/manager-master.wazuh-test.com/'${ubuntu_manager_ip}'/g' $development_agent_path
+for dist in "${distributions[@]}"
+do
+	echo "Kitchen is creating the new instances with dist=$dist"
+	kitchen create ubuntu
+
+	echo "Getting Wazuh managers IPs to the agents"
+	manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep  $dist | grep manager`)"
 
 
-echo "Kitchen is converging ..."
-kitchen converge ubuntu
+	cp "$development_agent_path$template" "$development_agent_path"
+
+	echo "Assigning Wazuh managers IPs to the corresponding agents."
+	sed -i 's/manager-client.wazuh-test.com//g' $development_agent_path
+	sed -i 's/manager-master.wazuh-test.com/'${manager_ip}'/g' $development_agent_path
+
+
+	echo "Kitchen is converging ..."
+	kitchen converge $dist
+
+
+	echo "Getting default things back"
+	cp "$development_agent_path$template" "$development_agent_path"
+done
 
 echo "Kitchen is testing ..."
-kitchen verify ubuntu
-
-echo "Getting default things back"
-cp "$development_agent_path$template" "$development_agent_path"
+kitchen verify
