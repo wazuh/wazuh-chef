a328b2775afbb6529d844819bbb8a3aeb56c90ab
Rshad Zhran <rashzk95@gmail.com>
2019-08-12 11:53:11 +0200

    changed cookbook name from java to wazuh-elastic

--- cookbooks/wazuh_elastic/recipes/oracle.rb
+++ cookbooks/wazuh_elastic/recipes/oracle.rb
@@ -17,46 +17,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'java::notify'
+include_recipe 'wazuh-elastic::notify'
 
-unless node.recipe?('java::default')
-  Chef::Log.warn('Using java::default instead is recommended.')
+unless node.recipe?('wazuh-elastic::default')
+  Chef::Log.warn('Using wazuh-elastic::default instead is recommended.')
 
   # Even if this recipe is included by itself, a safety check is nice...
-  if node['java']['java_home'].nil? || node['java']['java_home'].empty?
-    include_recipe 'java::set_attributes_from_version'
+  if node['wazuh-elastic']['java_home'].nil? || node['wazuh-elastic']['java_home'].empty?
+    include_recipe 'wazuh-elastic::set_attributes_from_version'
   end
 end
 
-java_home = node['java']['java_home']
-arch = node['java']['arch']
-version = node['java']['jdk_version'].to_s
-tarball_url = node['java']['jdk'][version][arch]['url']
-tarball_checksum = node['java']['jdk'][version][arch]['checksum']
-bin_cmds = node['java']['jdk'][version]['bin_cmds']
+java_home = node['wazuh-elastic']['java_home']
+arch = node['wazuh-elastic']['arch']
+version = node['wazuh-elastic']['jdk_version'].to_s
+tarball_url = node['wazuh-elastic']['jdk'][version][arch]['url']
+tarball_checksum = node['wazuh-elastic']['jdk'][version][arch]['checksum']
+bin_cmds = node['wazuh-elastic']['jdk'][version]['bin_cmds']
 
-include_recipe 'java::set_java_home'
+include_recipe 'wazuh-elastic::set_java_home'
 
 java_oracle_install 'jdk' do
   url tarball_url
-  default node['java']['set_default']
+  default node['wazuh-elastic']['set_default']
   checksum tarball_checksum
   app_home java_home
   bin_cmds bin_cmds
-  alternatives_priority node['java']['alternatives_priority']
-  retries node['java']['ark_retries']
-  retry_delay node['java']['ark_retry_delay']
-  connect_timeout node['java']['ark_timeout']
-  use_alt_suffix node['java']['use_alt_suffix']
-  reset_alternatives node['java']['reset_alternatives']
-  download_timeout node['java']['ark_download_timeout']
-  proxy node['java']['ark_proxy']
+  alternatives_priority node['wazuh-elastic']['alternatives_priority']
+  retries node['wazuh-elastic']['ark_retries']
+  retry_delay node['wazuh-elastic']['ark_retry_delay']
+  connect_timeout node['wazuh-elastic']['ark_timeout']
+  use_alt_suffix node['wazuh-elastic']['use_alt_suffix']
+  reset_alternatives node['wazuh-elastic']['reset_alternatives']
+  download_timeout node['wazuh-elastic']['ark_download_timeout']
+  proxy node['wazuh-elastic']['ark_proxy']
   action :install
   notifies :write, 'log[jdk-version-changed]', :immediately
 end
 
-if node['java']['set_default'] && platform_family?('debian')
-  include_recipe 'java::default_java_symlink'
+if node['wazuh-elastic']['set_default'] && platform_family?('debian')
+  include_recipe 'wazuh-elastic::default_java_symlink'
 end
 
-include_recipe 'java::oracle_jce' if node['java']['oracle']['jce']['enabled']
+include_recipe 'wazuh-elastic::oracle_jce' if node['wazuh-elastic']['oracle']['jce']['enabled']
