1a0a6fb2f4d069ed316f43f989d3a47e74211656
Rshad Zhran <rashzk95@gmail.com>
2019-08-14 09:59:55 +0200

    adapted filebeat's module package link

--- cookbooks/wazuh_filebeat/attributes/versions.rb
+++ cookbooks/wazuh_filebeat/attributes/versions.rb
@@ -1,4 +1,4 @@
 default['filebeat']['elastic_stack_version'] = '7.2.0'
 default['filebeat']['wazuh_app_version'] = "3.9.4_7.2.0"
 default['filebeat']['extensions_version'] = "v3.9.4"
-
+default['filebeat']['module_package'] = "wazuh-filebeat-0.1.tar.gz"
