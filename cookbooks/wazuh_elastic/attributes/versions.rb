a7df65470aaeca1c0fa0094791c5ff415fccefb0
Rshad Zhran <rashzk95@gmail.com>
2019-08-21 13:50:37 +0200

    updated versions

--- cookbooks/wazuh_elastic/attributes/versions.rb
+++ cookbooks/wazuh_elastic/attributes/versions.rb
@@ -1,3 +1,3 @@
-default['wazuh-elastic']['elastic_stack_version'] = '7.2.0'
-default['wazuh-elastic']['wazuh_app_version'] = "3.9.3_7.2.0"
-default['wazuh-elastic']['extensions_version'] = "v3.9.3"
+default['wazuh-elastic']['elastic_stack_version'] = '7.3.0'
+default['wazuh-elastic']['wazuh_app_version'] = "3.10_7.3.0"
+default['wazuh-elastic']['extensions_version'] = "v3.10"
--- cookbooks/wazuh_filebeat/attributes/versions.rb
+++ cookbooks/wazuh_filebeat/attributes/versions.rb
@@ -1,4 +1,4 @@
-default['filebeat']['elastic_stack_version'] = '7.2.0'
-default['filebeat']['wazuh_app_version'] = "3.9.3_7.2.0"
-default['filebeat']['extensions_version'] = "v3.9.3"
+default['filebeat']['elastic_stack_version'] = '7.3.0'
+default['filebeat']['wazuh_app_version'] = "3.10_7.3.0"
+default['filebeat']['extensions_version'] = "v3.10"
 
