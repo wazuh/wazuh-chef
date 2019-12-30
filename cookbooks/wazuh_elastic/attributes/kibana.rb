1f9743c5e50d7b9f75436bbd53c075cc68ed130a
Jose M <jm.garcia@wazuh.com>
2019-12-30 18:23:15 +0100

    Add required Wazuh API credentials in Kibana default attributes

--- cookbooks/wazuh_elastic/attributes/kibana.rb
+++ cookbooks/wazuh_elastic/attributes/kibana.rb
@@ -1,3 +1,4 @@
 default['wazuh-elastic']['kibana_server_host'] = '0.0.0.0'
 default['wazuh-elastic']['kibana_server_port'] = '5601'
 default['wazuh-elastic']['kibana_elasticsearch_server_hosts'] = "http://#{node['wazuh-elastic']['elasticsearch_ip']}:#{node['wazuh-elastic']['elasticsearch_port']}"
+default['wazuh-elastic']['kibana_api_credentials'] = [ { id: "default", url: "http://localhost", port: "55000", user: "foo", password: "bar" } ]
