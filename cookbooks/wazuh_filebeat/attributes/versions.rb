8bf7f3b890034f4139582d8dbed20a4aaf689365
Rshad Zhran <rashzk95@gmail.com>
2020-01-07 11:22:09 +0100

    Bump version - elk = 7.5.1

--- cookbooks/wazuh_filebeat/attributes/versions.rb
+++ cookbooks/wazuh_filebeat/attributes/versions.rb
@@ -1,4 +1,4 @@
-default['filebeat']['elastic_stack_version'] = '7.3.2'
-default['filebeat']['wazuh_app_version'] = "3.11.1_7.3.2"
+default['filebeat']['elastic_stack_version'] = '7.5.1'
+default['filebeat']['wazuh_app_version'] = "3.11.1_7.5.1"
 default['filebeat']['extensions_version'] = "v3.11.1"
 default['filebeat']['wazuh_filebeat_module'] = "wazuh-filebeat-0.1.tar.gz"
