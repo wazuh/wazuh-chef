a973b7cd73800f501ba73d64c877cdaa1727d311
Jose M <jm.garcia@wazuh.com>
2020-01-07 16:30:04 +0100

    Restore Wazuh API credentials to default value

--- cookbooks/wazuh_elastic/attributes/kibana.rb
+++ cookbooks/wazuh_elastic/attributes/kibana.rb
@@ -1,4 +1,4 @@
 default['wazuh-elastic']['kibana_server_host'] = '0.0.0.0'
 default['wazuh-elastic']['kibana_server_port'] = '5601'
 default['wazuh-elastic']['kibana_elasticsearch_server_hosts'] = "http://#{node['wazuh-elastic']['elasticsearch_ip']}:#{node['wazuh-elastic']['elasticsearch_port']}"
-default['wazuh-elastic']['kibana_wazuh_api_credentials'] = [ { id: "production", url: "http://172.23.0.101", port: "55000", user: "test", password: "test2" } ]
+default['wazuh-elastic']['kibana_wazuh_api_credentials'] = [ { id: "default", url: "http://localhost", port: "55000", user: "foo", password: "bar" } ]
