4f7563ce89c5c481cb8d69a1fa35c48156e3b8c6
Rshad Zhran <rashzk95@gmail.com>
2020-01-07 11:56:43 +0100

    Bump version - Wazuh = 3.11.0_7.5.1

--- cookbooks/wazuh_filebeat/attributes/versions.rb
+++ cookbooks/wazuh_filebeat/attributes/versions.rb
@@ -1,4 +1,4 @@
 default['filebeat']['elastic_stack_version'] = '7.5.1'
-default['filebeat']['wazuh_app_version'] = "3.11.0_7.5.1"
-default['filebeat']['extensions_version'] = "v3.11.0"
+default['filebeat']['wazuh_app_version'] = "3.11.1_7.5.1"
+default['filebeat']['extensions_version'] = "v3.11.1"
 default['filebeat']['wazuh_filebeat_module'] = "wazuh-filebeat-0.1.tar.gz"
