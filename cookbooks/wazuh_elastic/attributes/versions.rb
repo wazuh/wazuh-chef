c76695a23136ab529aca5e96e05e0e1b3c15d4f3
Rshad Zhran <rashzk95@gmail.com>
2020-01-07 11:20:59 +0100

    Bump version => wazuh = 3.11.1

--- cookbooks/wazuh_elastic/attributes/versions.rb
+++ cookbooks/wazuh_elastic/attributes/versions.rb
@@ -1,3 +1,3 @@
 default['wazuh-elastic']['elastic_stack_version'] = '7.3.2'
-default['wazuh-elastic']['wazuh_app_version'] = "3.10.2_7.3.2"
-default['wazuh-elastic']['extensions_version'] = "v3.10.2"
+default['wazuh-elastic']['wazuh_app_version'] = "3.11.1_7.3.2"
+default['wazuh-elastic']['extensions_version'] = "v3.11.1"
