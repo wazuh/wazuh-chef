9fc1a224312b658ebd3bc123a0af6c839260e310
Sergio García Prados <garcapradossergi76@gmail.com>
2020-11-06 14:54:48 +0100

    Update filebeat repository

--- cookbooks/wazuh_filebeat/recipes/repository.rb
+++ cookbooks/wazuh_filebeat/recipes/repository.rb
@@ -14,20 +14,35 @@ if platform_family?('debian','ubuntu')
   end
 
  apt_repository "elastic-7.x" do
-    uri 'https://artifacts.elastic.co/packages/7.x/apt'
+    description 'Filebeat OSS 7.x Apt Repository'
+    uri 'https://artifacts.elastic.co/packages/oss-7.x/apt'
     key 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
     distribution "stable"
     components ["main"]
+    action :create
     not_if do
       File.exists?("/etc/apt/sources.list.d/elastic-7.x.list")
     end
   end
 elsif platform_family?('rhel', 'redhat', 'centos', 'amazon')
   yum_repository "elastic-7.x" do
-    description "Elastic repository for 7.x packages"
-    baseurl "https://artifacts.elastic.co/packages/7.x/yum"
+    description 'Filebeat OSS 7.x Yum Repository'
+    baseurl "https://artifacts.elastic.co/packages/oss-7.x/yum"
     gpgkey "https://artifacts.elastic.co/GPG-KEY-elasticsearch"
     action :create
+    not_if do
+      File.exists?("/etc/yum/sources.list.d/elastic-7.x.list")
+    end
+  end
+elsif platform_family?('suse')
+  zypper_repository "elastic-7.x" do
+    description 'Filebeat OSS 7.x Zypper Repository'
+    baseurl "https://artifacts.elastic.co/packages/oss-7.x/yum"
+    gpgkey "https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+    action :create
+    not_if do
+      File.exists?("/etc/apt/sources.list.d/elastic-7.x.list")
+    end
   end
 else
   raise "Currently platforn not supported yet. Feel free to open an issue on https://www.github.com/wazuh/wazuh-chef if you consider that support for a specific OS should be added"
