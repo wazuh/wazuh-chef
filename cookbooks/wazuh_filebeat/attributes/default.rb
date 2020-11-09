93a579cbb1bde2b14cf2712e7f2c11ca3ee3f0f7
Sergio García Prados <garcapradossergi76@gmail.com>
2020-11-09 13:38:58 +0100

    default.tb

--- cookbooks/wazuh_filebeat/attributes/default.rb
+++ cookbooks/wazuh_filebeat/attributes/default.rb
@@ -10,4 +10,4 @@ default['filebeat']['service_name'] = 'filebeat'
 default['filebeat']['elasticsearch_server_ip'] = "localhost"
 default['filebeat']['timeout'] = 15
 default['filebeat']['config_path'] = '/etc/filebeat/filebeat.yml'
-
+default['filebeat']['elasticsearch_server_port'] = 9200
