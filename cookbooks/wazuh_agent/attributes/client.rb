19cf19ab588ae6ed30c45d4dc3230eea31981dd1
Sergio García Prados <garcapradossergi76@gmail.com>
2020-11-13 13:42:18 +0100

    Change client.rb syntax

--- cookbooks/wazuh_agent/attributes/client.rb
+++ cookbooks/wazuh_agent/attributes/client.rb
@@ -1,36 +1,52 @@
 # <client> ... </client> ossec.conf variables
 
-# Server subsection <server> ... </server> 
+# <client>
+#   <server> 
+#       ... 
+#   </server> 
+# </client> 
 
-default['ossec']['conf']['client']['server']['address'] = node['ossec']['address']
-default['ossec']['conf']['client']['server']['port'] = 1514
-default['ossec']['conf']['client']['server']['protocol'] = 'udp'
-default['ossec']['conf']['client']['server']['max_retries'] = '5'
-default['ossec']['conf']['client']['server']['retry_interval'] = '5'
+default['ossec']['conf']['client']['server'] = [{
+        'address' => node['ossec']['address'],
+        'port' => "1514",
+        'protocol' => "tcp",
+        'max_retries' => "5",
+        'retry_interval' => "5"
+}]
 
-# Client subvariables
+# <client>
+#   ...
+# </client>
 
-default['ossec']['conf']['client']['config-profile'] = ""
-default['ossec']['conf']['client']['notify_time'] = 10
-default['ossec']['conf']['client']['time-reconnect'] = 60
-default['ossec']['conf']['client']['local_ip'] = ""
-default['ossec']['conf']['client']['disable-active-response'] = false
-default['ossec']['conf']['client']['auto_restart'] = true
-default['ossec']['conf']['client']['crypto_method'] = "aes"
+default['ossec']['conf']['client'] = [{
+    # 'config-profile' => ""    
+    'notify_time' => "10",
+    'time-reconnect' => "60",
+    # 'local_ip' => "",
+    'disable-active-response' => "no",
+    'auto_restart' => "yes",
+    'crypto_method' => "aes"
+}]                                
 
-# Enrollment subsection <enrollment> ... </enrollment>
+# <client>
+#   <enrollment> 
+#       ... 
+#   </enrollment> 
+# </client> 
 
-default['ossec']['conf']['client']['enrollment']['enabled'] = true
-default['ossec']['conf']['client']['enrollment']['manager_address'] = ""
-default['ossec']['conf']['client']['enrollment']['port'] = 1515
-default['ossec']['conf']['client']['enrollment']['agent_name'] = ""
-default['ossec']['conf']['client']['enrollment']['groups'] = ""
-default['ossec']['conf']['client']['enrollment']['agent_address'] = ""
-default['ossec']['conf']['client']['enrollment']['ssl_cipher'] = "HIGH:!ADH:!EXP:!MD5:!RC4:!3DES:!CAMELLIA:@STRENGTH"
-default['ossec']['conf']['client']['enrollment']['server_ca_path'] = ""
-default['ossec']['conf']['client']['enrollment']['agent_certificate_path'] = "" 
-default['ossec']['conf']['client']['enrollment']['agent_key_path'] = ""
-default['ossec']['conf']['client']['enrollment']['authorization_pass_path'] = ""
-default['ossec']['conf']['client']['enrollment']['auto_method'] = false
-default['ossec']['conf']['client']['enrollment']['delay_after_enrollment'] = 20
-default['ossec']['conf']['client']['enrollment']['use_source_ip'] = false
+default['ossec']['conf']['client']['enrollment'] = [{
+    'enabled' => "yes",
+    # 'manager_address' => "",
+    'port' => "1515",
+    # 'agent_name' => "",
+    # 'groups' => "",
+    # 'agent_address' => "",
+    'ssl_cipher' => "HIGH:!ADH:!EXP:!MD5:!RC4:!3DES:!CAMELLIA:@STRENGTH",
+    # 'server_ca_path' => "",
+    # 'agent_certificate_path' => "", 
+    # 'agent_key_path' => "",
+    # 'authorization_pass_path' => "",
+    'auto_method' => "no",
+    'delay_after_enrollment' => "20",
+    'use_source_ip' => "no"
+}]
