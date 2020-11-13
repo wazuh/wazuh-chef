9218fb870f216a0dc04429067bf936b334623d31
Sergio García Prados <garcapradossergi76@gmail.com>
2020-11-13 13:46:06 +0100

    Change quote

--- cookbooks/wazuh_agent/attributes/client.rb
+++ cookbooks/wazuh_agent/attributes/client.rb
@@ -8,10 +8,10 @@
 
 default['ossec']['conf']['client']['server'] = [{
         'address' => node['ossec']['address'],
-        'port' => "1514",
-        'protocol' => "tcp",
-        'max_retries' => "5",
-        'retry_interval' => "5"
+        'port' => '1514',
+        'protocol' => 'tcp',
+        'max_retries' => '5',
+        'retry_interval' => '5'
 }]
 
 # <client>
@@ -19,13 +19,13 @@ default['ossec']['conf']['client']['server'] = [{
 # </client>
 
 default['ossec']['conf']['client'] = [{
-    # 'config-profile' => ""    
-    'notify_time' => "10",
-    'time-reconnect' => "60",
-    # 'local_ip' => "",
-    'disable-active-response' => "no",
-    'auto_restart' => "yes",
-    'crypto_method' => "aes"
+    # 'config-profile' => ''    
+    'notify_time' => '10',
+    'time-reconnect' => '60',
+    # 'local_ip' => '',
+    'disable-active-response' => 'no',
+    'auto_restart' => 'yes',
+    'crypto_method' => 'aes'
 }]                                
 
 # <client>
@@ -35,18 +35,18 @@ default['ossec']['conf']['client'] = [{
 # </client> 
 
 default['ossec']['conf']['client']['enrollment'] = [{
-    'enabled' => "yes",
-    # 'manager_address' => "",
-    'port' => "1515",
-    # 'agent_name' => "",
-    # 'groups' => "",
-    # 'agent_address' => "",
-    'ssl_cipher' => "HIGH:!ADH:!EXP:!MD5:!RC4:!3DES:!CAMELLIA:@STRENGTH",
-    # 'server_ca_path' => "",
-    # 'agent_certificate_path' => "", 
-    # 'agent_key_path' => "",
-    # 'authorization_pass_path' => "",
-    'auto_method' => "no",
-    'delay_after_enrollment' => "20",
-    'use_source_ip' => "no"
+    'enabled' => 'yes',
+    # 'manager_address' => '',
+    'port' => '1515',
+    # 'agent_name' => '',
+    # 'groups' => '',
+    # 'agent_address' => '',
+    'ssl_cipher' => 'HIGH:!ADH:!EXP:!MD5:!RC4:!3DES:!CAMELLIA:@STRENGTH',
+    # 'server_ca_path' => '',
+    # 'agent_certificate_path' => '', 
+    # 'agent_key_path' => '',
+    # 'authorization_pass_path' => '',
+    'auto_method' => 'no',
+    'delay_after_enrollment' => '20',
+    'use_source_ip' => 'no'
 }]
