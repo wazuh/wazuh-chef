da457eca7f167258f5b7a1d427dad712fe9383e8
Sergio García Prados <garcapradossergi76@gmail.com>
2020-11-27 11:15:53 +0100

    Change attribute files name

---
+++ cookbooks/filebeat/attributes/elastic.rb
@@ -0,0 +1,8 @@
+# Cookbook Name:: filebeat
+# Attribute:: elastic
+# Author:: Wazuh <info@pwazuh.com>
+
+default['filebeat']['elasticsearch_server_ip'] = [
+    "http://0.0.0.0:9200"
+]
+
---
+++ cookbooks/filebeat/attributes/paths.rb
@@ -0,0 +1,6 @@
+# Cookbook Name:: filebeat
+# Attribute:: paths
+# Author:: Wazuh <info@pwazuh.com>
+
+default['filebeat']['config_path'] = '/etc/filebeat'
+default['filebeat']['wazuh_module_path'] = '/usr/share/filebeat/module'
