c9632628ac0c11905b2e179c60ac9a78772229f7
Sergio García Prados <garcapradossergi76@gmail.com>
2020-11-27 15:21:12 +0100

    Move kibana yml attribute

--- cookbooks/opendistro/attributes/api.rb
+++ cookbooks/opendistro/attributes/api.rb
@@ -1,12 +1,2 @@
-default['kibana']['yml'] = {
-    'server' => {
-        'host' => '0.0.0.0',
-        'port' => 5601
-    },
-    'elasticsearch' => {
-        'hosts' => [
-            "https://#{node['wazuh-elastic']['yml']['network']['host']}:#{node['wazuh-elastic']['yml']['http']['port']}"
-        ]
-    }
-}
+
 default['wazuh-elastic']['kibana_wazuh_api_credentials'] = [ { id: "default", url: "https://localhost", port: "55000", username: "wazuh", password: "wazuh" } ]
