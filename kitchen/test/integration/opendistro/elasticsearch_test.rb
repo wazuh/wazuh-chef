f767373bb328b0bd331fd4184e4d4e4c36d36588
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-14 14:10:20 +0100

    Add kitchen folder with all tests

---
+++ kitchen/test/integration/opendistro/elasticsearch_test.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+describe packages(/elasticsearch-oss/) do
+  its('statuses') { should cmp 'installed' }
+end
+
+describe packages(/opendistroforelasticsearch/) do
+  its('statuses') { should cmp 'installed' }
+end
+
+describe elasticsearch do
+  its('node_name') { should cmp 'es-node-01' }
+  its('cluster_name') { should cmp 'es-wazuh' }
+  its('url') { should cmp 'http://localhost:9200' }
+end
+
+describe directory '/etc/elasticsearch' do
+  its('owner') { should cmp 'elasticsearch' }
+  its('group') { should cmp 'elasticsearch' }
+end
+
+describe directory '/usr/share/elasticsearch' do
+  its('owner') { should cmp 'elasticsearch' }
+  its('group') { should cmp 'elasticsearch' }
+end
+
+describe directory '/var/lib/elasticsearch' do
+  its('owner') { should cmp 'elasticsearch' }
+  its('group') { should cmp 'elasticsearch' }
+end
+
+describe file('/etc/elasticsearch/elasticsearch.yml') do
+  its('owner') { should cmp 'root' }
+  its('group') { should cmp 'elasticsearch' }
+  its('mode') { should cmp '0660' }
+end
+
+describe file('/usr/share/elasticsearch/plugins/opendistro_security/securityconfig/roles.yml') do
+  it { should exist }
+end
+
+describe file('/usr/share/elasticsearch/plugins/opendistro_security/securityconfig/roles_mapping.yml') do
+  it { should exist }
+end
+
+describe file('/usr/share/elasticsearch/plugins/opendistro_security/securityconfig/internal_users.yml') do
+  it { should exist }
+end
+
+describe file('/etc/searchguard/search-guard.yml') do
+  its('owner') { should cmp 'root' }
+  its('group') { should cmp 'elasticsearch' }
+  its('mode') { should cmp '0660' }
+end
+
+describe file('/etc/elasticsearch/root-ca.pem') do
+  it { should exist }
+end
+
+describe service('elasticsearch') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(9200) do
+  it { should be_listening }
+end
---
+++ kitchen/test/integration/opendistro/prerequisites_test.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+describe 'elastic-stack::prerequisites' do
+  describe package('curl') do
+    it { should be_installed }
+  end
+
+  describe package('unzip') do
+    it { should be_installed }
+  end
+
+  describe package('wget') do
+    it { should be_installed }
+  end
+
+  case os.family
+  when 'debian'
+    describe package('apt-transport-https') do
+      it { should be_installed }
+    end
+
+    describe package('software-properties-common') do
+      it { should be_installed }
+    end
+
+    describe package('libcap2-bin') do
+      it { should be_installed }
+    end
+
+    describe package('openjdk-11-jdk') do
+      it { should be_installed }
+    end
+
+  when 'redhat'
+    describe package('libcap') do
+      it { should be_installed }
+    end
+
+    describe package('java-11-openjdk-devel') do
+      it { should be_installed }
+    end
+  when 'suse'
+    describe package('libcap2') do
+      it { should be_installed }
+    end
+
+    describe package('java-11-openjdk-devel') do
+      it { should be_installed }
+    end
+  end
+end
