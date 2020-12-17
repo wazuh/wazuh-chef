b89deb490d5c0d27d8a197ea287b1deff17fc833
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-17 12:11:35 +0100

    Add fail test

--- .github/actions/failure.sh
+++ .github/actions/failure.sh
@@ -1,18 +1,9 @@
 #! /usr/bin/env bash
 set -e
 
-case $OS in
-    ubuntu-18.04)
-        OS_NAME=ubuntu-1804
-        ;;
-    ubuntu-20.04)
-        OS_NAME=ubuntu-2004
-        ;;
-    *)
-        OS_NAME=$OS
-        ;;
-esac
+cd cookbooks/$COOKBOOK_NAME
 
-LOG_FILE=elastic-stack-$OS_NAME.log
+LOG_FILE=$COOKBOOK_NAME-$OS.log
+
 echo "Print out logs"
 cat .kitchen/logs/$LOG_FILE
