262f755cf81cf531d2c8808252e1b622738149b3
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-17 11:48:54 +0100

    Update gh action

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,25 +1,12 @@
 #! /usr/bin/env bash
 set -e
 
-echo "Cookbook to test: "
-echo $COOKBOOK_NAME
+echo "Cookbook to test: ${COOKBOOK_NAME}"
 
 cd cookbooks/$COOKBOOK_NAME
 
-echo "Installing dependencies"
+echo "Installing dependencies..."
 bundle install
 
-case $OS in
-    ubuntu-18.04)
-        OS_NAME=ubuntu-1804
-        ;;
-    ubuntu-20.04)
-        OS_NAME=ubuntu-2004
-        ;;
-    *)
-        OS_NAME=$OS
-        ;;
-esac
-
 echo "Test cookbook with kitchen... (create, converge, verify and destroy)"
-kitchen test $COOKBOOK_NAME-$OS_NAME
+kitchen test $COOKBOOK_NAME-$OS
