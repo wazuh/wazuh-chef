cd29d7c2004688a3ff4023e1b3f814e72ca49da5
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-18 13:01:58 +0100

    Add inspec test wazuh-manager

---
+++ cookbooks/wazuh_manager/test/integration/wazuh-manager/manager_test.rb
@@ -0,0 +1,22 @@
+describe package('wazuh-manager') do
+    it { should be_installed }
+end
+
+describe service('wazuh-manager') do
+    it { should be_running }
+end
+
+describe port(55000) do
+    it { should be_listening }
+    its('processes') {should include 'python3'}
+end
+
+describe port(1515) do
+    it { should be_listening }
+    its('processes') {should include 'ossec-authd'}
+end
+
+describe port(1514) do
+    it { should be_listening }
+    its('processes') {should include 'ossec-remoted'}
+end
