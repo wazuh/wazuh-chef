30ef9b11c7d56604e9bec5c51c4b36117b48690a
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-21 12:13:49 +0100

    Fix apt url

--- cookbooks/filebeat/test/integration/filebeat/repository_test.rb
+++ cookbooks/filebeat/test/integration/filebeat/repository_test.rb
@@ -2,7 +2,7 @@
 
 case os.family
 when 'debian'
-    describe apt('https://artifacts.elastic.co/packages/7.x/apt/') do
+    describe apt('https://artifacts.elastic.co/packages/7.x/apt') do
         it { should exist }
         it { should be_enabled }
     end
