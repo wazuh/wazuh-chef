1d04becc2ddbe2d7317ca0c8d064d198bd3f317e
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-22 16:37:36 +0100

    Remove action folder

--- .github/actions/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-set -e
-
-echo "Cookbook to test: ${COOKBOOK_NAME}"
-
-echo "Test cookbook with kitchen... (create, converge, verify and destroy)"
-kitchen test $COOKBOOK_NAME-$OS
--- .github/actions/failure.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-set -e
-
-LOG_FILE=$COOKBOOK_NAME-$OS.log
-
-echo "Print out logs"
-cat ~/wazuh-chef/.kitchen/logs/$LOG_FILE
