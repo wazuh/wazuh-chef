f061751502322ef6085547b8ccf00fc3f6ab7cf3
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-28 14:02:12 +0100

    Remove white space

--- cookbooks/wazuh_agent/attributes/authd.rb
+++ cookbooks/wazuh_agent/attributes/authd.rb
@@ -19,9 +19,8 @@
 
 default['ossec']['agent_auth']['register'] = 'yes'
 default['ossec']['agent_auth']['name'] = node['hostname']
-default['ossec']['agent_auth']['host'] = node['ossec']['registration_address']
+default['ossec']['agent_auth']['host'] = node['ossec']['address']
 default['ossec']['agent_auth']['port'] = 1515
-
 default['ossec']['agent_auth']['ca'] = nil
 default['ossec']['agent_auth']['certificate'] = nil
 default['ossec']['agent_auth']['key'] = nil
