e6ff1fe36440f088a02c6235fedef0049f820235
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-29 20:02:06 +0100

    Check service is installed, enabled and running

--- cookbooks/opendistro/test/integration/opendistro/elasticsearch_test.rb
+++ cookbooks/opendistro/test/integration/opendistro/elasticsearch_test.rb
@@ -58,7 +58,10 @@ describe file('/etc/elasticsearch/certs/root-ca.pem') do
 end
 
 describe service('elasticsearch') do
+  it { should be_installed }
+  it { should be_enabled}
   it { should be_running }
+
 end
 
 describe port(9200) do
