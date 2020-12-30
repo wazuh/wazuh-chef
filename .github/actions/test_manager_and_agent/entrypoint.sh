c75bcb08049f1150d77cda3ee2074c7fc6a7e8b5
Sergio García Prados <garcapradossergi76@gmail.com>
2020-12-30 14:21:16 +0100

    Fix bash command and initialize MANAGER_IP

--- .github/actions/test_manager_and_agent/entrypoint.sh
+++ .github/actions/test_manager_and_agent/entrypoint.sh
@@ -20,15 +20,17 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 echo "Kitchen create manager..."
-kitchen create wazuh-manager-$PLATFORM-$RELEASE
+kitchen create wazuh-server-$PLATFORM-$RELEASE
 
-container-id="$(docker ps -aqf \"name=wazuh-manager-$PLATFORM-$RELEASE$\")"
+echo "Getting wazuh-server-$PLATFORM-$RELEASE container ID"
+container_id="$(docker ps -aqf "name=wazuh-server-$PLATFORM-$RELEASE$")"
 
-echo "Getting Wazuh managers IPs to the agents"
+echo "Getting Wazuh managers IP"
 manager_ip="$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)"
 
 echo "wazuh-manager IP"
 echo $manager_ip
+MANAGER_IP=$manager_ip
 
 echo "Kitchen converge manager ..."
 kitchen converge wazuh-manager-$PLATFORM-$RELEASE
@@ -42,9 +44,8 @@ kitchen create wazuh-agent-$PLATFORM-$RELEASE
 echo "Kitchen converge agent..."
 kitchen converge wazuh-agent-$PLATFORM-$RELEASE
 
-echo "Change IP manager address in ossec.conf..."
-kitchen exec wazuh-agent-$PLATFORM-$RELEASE -c "sed -i \"s/<address>*<\/address>/<address>${manager_ip}<\/address>/g\" ossec.conf" 
-
-echo "Connect agent with manager"
-kitchen exec 
-/bin/bash
+#echo "Change IP manager address in ossec.conf..."
+#kitchen exec wazuh-agent-$PLATFORM-$RELEASE -c "sed -i \"s/<address>*<\/address>/<address>${manager_ip}<\/address>/g\" ossec.conf" 
+#
+#echo "Connect agent with manager"
+#kitchen converge
