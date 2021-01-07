408cbac094397316101143ed149ff4cf0ad66b34
Sergio García <garcapradossergi76@gmail.com>
2021-01-07 16:10:53 +0100

    ruleset.rb: add more variables to ruleset

--- cookbooks/wazuh_manager/attributes/ruleset.rb
+++ cookbooks/wazuh_manager/attributes/ruleset.rb
@@ -3,19 +3,27 @@
 # Author:: Wazuh <info@wazuh.com
 
 # Ruleset settings (Manager)
-default['ossec']['conf']['ruleset'] = {
-    'decoder_dir' => [
-        'ruleset/decoders', 
-        'etc/decoders'
-    ],
-    'rule_dir' => [
-        'ruleset/rules', 
-        'etc/rules'
-    ],
-    'rule_exclude' => '0215-policy_rules.xml',
-    'list' => [
-        'etc/lists/audit-keys', 
-        'etc/lists/security-eventchannel', 
-        'etc/lists/amazon/aws-eventnames'
-    ]
-}
+default['ossec']['conf']['ruleset'] = [
+    {
+        'decoder_dir' => [
+            'ruleset/decoders', 
+            'etc/decoders'
+        ],
+        'rule_dir' => [
+            'ruleset/rules', 
+            'etc/rules'
+        ],
+        'rule_exclude' => '0215-policy_rules.xml',
+        'list' => [
+            'etc/lists/audit-keys', 
+            'etc/lists/security-eventchannel', 
+            'etc/lists/amazon/aws-eventnames'
+        ]
+    },
+    {
+        'enabled' => true,
+        'threads' => 1,
+        'max_sessions' => 64,
+        'session_timeout' => '15m'
+    }
+]
