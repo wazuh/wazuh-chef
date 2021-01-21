453bed89a0863699583f86ae36f3ac2b7b56d2b0
Sergio García <garcapradossergi76@gmail.com>
2021-01-21 10:58:43 +0100

    manager_agent.rb: Check agent ossec.log is reporting to manager

--- test/manager_agent/manager_agent.rb
+++ test/manager_agent/manager_agent.rb
@@ -3,3 +3,8 @@
 describe command("netstat -vatunp | grep ossec-agentd | awk \'{print $5}\'") do
     its('stdout') { should match ('172.16.100.10:1514')} 
 end
+
+# Check agent reports logs from manager in ossec.log
+describe command("cat /var/ossec/logs/ossec.log | grep \"Connected to the server\" | awk \'{print $6, $7, $8, $9, $10}\' | uniq -d") do
+    its('stdout') { should match ("Connected to the server (172.16.100.10:1514/tcp).\n")}
+end
