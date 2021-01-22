b6596c6b45167a298719177ad218963a96d4356e
Sergio García <garcapradossergi76@gmail.com>
2021-01-22 11:58:57 +0100

    manager_agent_test.rb: change manager and agent connection test path

--- cookbooks/wazuh_agent/test/integration/wazuh_agent/manager_agent_test.rb
+++ cookbooks/wazuh_agent/test/integration/wazuh_agent/manager_agent_test.rb
@@ -1,10 +1,11 @@
-# Check agent and manager connection
+# Check agent is listening to manager
 
 describe command("netstat -vatunp | grep ossec-agentd | awk \'{print $5}\'") do
-    its('stdout') { should match ('172.16.100.10:1514')} 
+    its('stdout') { should match ("#{input('manager_ip')}:1514")} 
 end
 
 # Check agent reports logs from manager in ossec.log
+
 describe command("cat /var/ossec/logs/ossec.log | grep \"Connected to the server\" | awk \'{print $6, $7, $8, $9, $10}\' | uniq -d") do
-    its('stdout') { should match ("Connected to the server (172.16.100.10:1514/tcp).\n")}
+    its('stdout') { should match ("Connected to the server (#{input('manager_ip')}:1514/tcp).\n")}
 end
