5d3697c5d4802cd160e735022ae306c5d2837ec2
Sergio García <garcapradossergi76@gmail.com>
2021-01-25 10:52:45 +0100

    manager_agent_test.rb: add before resource (sleep 10)

--- cookbooks/wazuh_agent/test/integration/wazuh_agent/manager_agent_test.rb
+++ cookbooks/wazuh_agent/test/integration/wazuh_agent/manager_agent_test.rb
@@ -7,5 +7,9 @@ end
 # Check agent reports logs from manager in ossec.log
 
 describe command("cat /var/ossec/logs/ossec.log | grep \"Connected to the server\" | awk \'{print $6, $7, $8, $9, $10}\' | uniq -d") do
+    before do
+        puts "Sleeping 10 seconds..."
+        sleep 10
+    end
     its('stdout') { should match ("Connected to the server (#{input('manager_ip')}:1514/tcp).\n")}
 end
