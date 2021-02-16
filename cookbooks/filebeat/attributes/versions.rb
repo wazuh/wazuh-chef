85994ffb7817f7b74cc66db09350add444f77e8e
Sergio García <garcapradossergi76@gmail.com>
2021-02-16 16:26:03 +0100

    versions.rb: return to 4.0.4

--- cookbooks/filebeat/attributes/versions.rb
+++ cookbooks/filebeat/attributes/versions.rb
@@ -4,9 +4,9 @@
 
 # ELK 
 default['elk']['major_version'] = "7.x" 
-default['elk']['patch_version'] = "7.10.2"
+default['elk']['patch_version'] = "7.9.3"
 
 # Wazuh
 default['wazuh']['major_version'] = "4.x"
-default['wazuh']['minor_version'] = "4.1"
-default['wazuh']['patch_version'] = "4.1.0"
+default['wazuh']['minor_version'] = "4.0"
+default['wazuh']['patch_version'] = "4.0.4"
