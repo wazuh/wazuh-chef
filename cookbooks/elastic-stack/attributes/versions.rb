bdf3cb65bd7cc4883ebc3d9110ca0cc1bf3d442f
Sergio García <garcapradossergi76@gmail.com>
2021-02-19 20:21:04 +0100

    versions.rb: upgrade to wazuh v4.1.0

--- cookbooks/elastic-stack/attributes/versions.rb
+++ cookbooks/elastic-stack/attributes/versions.rb
@@ -6,12 +6,12 @@
 
 # ELK
 default['elk']['major_version'] = '7.x'
-default['elk']['patch_version'] = '7.9.3'
+default['elk']['patch_version'] = '7.10.2'
 
 # Wazuh
 default['wazuh']['major_version'] = '4.x'
-default['wazuh']['minor_version'] = '4.0'
-default['wazuh']['patch_version'] = '4.0.4'
+default['wazuh']['minor_version'] = '4.1'
+default['wazuh']['patch_version'] = '4.1.0'
 
 # Kibana
-default['wazuh']['kibana_plugin_version'] = '4.0.4_7.9.3'
+default['wazuh']['kibana_plugin_version'] = '4.1.0_7.10.2'
