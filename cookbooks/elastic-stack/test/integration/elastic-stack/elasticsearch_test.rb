434bbe10354befa6921ff17391b74c3a9a6969c1
Sergio García <garcapradossergi76@gmail.com>
2021-02-23 12:48:12 +0100

    Check os type and remove healt cluster check

--- cookbooks/elastic-stack/test/integration/elastic-stack/elasticsearch_test.rb
+++ cookbooks/elastic-stack/test/integration/elastic-stack/elasticsearch_test.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
-describe packages('elasticsearch') do
-  its('statuses') { should cmp 'installed' }
-  its('versions') { should cmp "#{input('elk_version')}-1" }
-end 
+if os.family == 'debian'
+  describe package('elasticsearch-oss') do
+    it { should be_installed }
+    its('version') { should eq "#{input('elk_version')}" }
+  end
+elsif os.family == 'redhat'
+  describe package('elasticsearch-oss') do
+    it { should be_installed }
+    its('version') { should eq "#{input('elk_version')}-1" }
+  end
+end
 
 describe file('/etc/elasticsearch/elasticsearch.yml') do
   its('owner') { should cmp 'elasticsearch' }
@@ -17,7 +24,7 @@ describe service('elasticsearch') do
   it { should be_running }
 end
 
-describe http("https://#{input('node_ip')}:#{input('elastic_port')}/_cluster/health?wait_for_status=green",
+describe http("https://#{input('node_ip')}:#{input('elastic_port')}",
               auth: {user: "#{input('elastic_user')}", pass: "#{input('elastic_password')}"},
               ssl_verify: false,
               method: 'GET') do
--- cookbooks/opendistro/test/integration/opendistro/elasticsearch_test.rb
+++ cookbooks/opendistro/test/integration/opendistro/elasticsearch_test.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
-describe package('elasticsearch-oss') do
-  it { should be_installed }
-  its('version') { should eq "#{input('elk_version')}-1" }
+if os.family == 'debian'
+  describe package('elasticsearch-oss') do
+    it { should be_installed }
+    its('version') { should eq "#{input('elk_version')}" }
+  end
+elsif os.family == 'redhat'
+  describe package('elasticsearch-oss') do
+    it { should be_installed }
+    its('version') { should eq "#{input('elk_version')}-1" }
+  end
 end
 
 describe package('opendistroforelasticsearch') do
   it { should be_installed }
   its('version') { should eq "#{input('odfe_version')}-1" }
-end
-
-describe elasticsearch do
-  its('node_name') { should cmp 'es-node-01' }
-  its('cluster_name') { should cmp 'es-wazuh' }
-  its('url') { should cmp 'http://localhost:9200' }
 end
 
 describe directory '/etc/elasticsearch' do
@@ -61,6 +62,9 @@ describe service('elasticsearch') do
 
 end
 
-describe port("#{input('elastic_port')}") do
-  it { should be_listening }
-end
+describe http("https://#{input('node_ip')}:#{input('elastic_port')}",
+              auth: {user: "#{input('elastic_user')}", pass: "#{input('elastic_password')}"},
+              ssl_verify: false,
+              method: 'GET') do
+  its('status') { should cmp 200 }
+end 
