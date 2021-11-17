4d2baa4d6393ada128f353b3f73fd1e488f4733a
Nicolas Lastra <lastra.nikos@gmail.com>
2021-11-17 15:43:13 -0300

    authd-update-in-ossec-conf

--- cookbooks/wazuh_manager/attributes/auth.rb
+++ cookbooks/wazuh_manager/attributes/auth.rb
@@ -7,8 +7,6 @@ default['ossec']['conf']['auth'] = {
     'disabled' => false,
     'port' => 1515,
     'use_source_ip' => false,
-    'force_insert' => true,
-    'force_time' => 0,
     'purge' => true,
     'use_password' => false,
     'limit_maxagents' => true,
@@ -17,4 +15,10 @@ default['ossec']['conf']['auth'] = {
     'ssl_manager_cert' => "#{node['ossec']['dir']}/etc/sslmanager.cert",
     'ssl_manager_key' => "#{node['ossec']['dir']}/etc/sslmanager.key",
     'ssl_auto_negotiate' => false
+    'force' => {
+        'enabled' => "yes"
+        'key_mismatch' => "yes"
+        'disconnected_time' => "1hs"
+        'after_registration_time' => "1hs"
+    },
 }
