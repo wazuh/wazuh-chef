0a94f24d1c78fadecf1e0179cfc453a0982e23d6
vcerenu <victor.erenu@wazuh.com>
2022-09-07 11:30:50 -0300

    bump 4.2.7 version

--- cookbooks/filebeat-oss/attributes/versions.rb
+++ cookbooks/filebeat-oss/attributes/versions.rb
@@ -2,11 +2,11 @@
 # Attribute:: versions
 # Author:: Wazuh <info@wazuh.com>
 
-# ELK 
-default['elk']['major_version'] = "7.x" 
+# ELK
+default['elk']['major_version'] = "7.x"
 default['elk']['patch_version'] = "7.10.2"
 
 # Wazuh
 default['wazuh']['major_version'] = "4.x"
 default['wazuh']['minor_version'] = "4.2"
-default['wazuh']['patch_version'] = "4.2.5"
+default['wazuh']['patch_version'] = "4.2.7"
